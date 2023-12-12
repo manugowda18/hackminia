ce0170e23e141d261e920f7060fb00e45846dfa9
manugowda18 <100668132+manugowda18@users.noreply.github.com>
2023-12-13 01:23:10 +0530

    sm update

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,7 +4,7 @@ import Marquee from "react-fast-marquee";
 const Announcement = () => {
     return (
         <div>
-            <Marquee className='mt-[-25px]'>
+            <Marquee className='sm:mt-[-25px] mt-[-40px]'>
                 <a href="" className='bg-black text-white px-16'>Announcement📢 : Click here to Download Guidelines and Code of Conduct</a>
                 <a href="" className='bg-black text-white px-16'>Announcement📢 : Click here to Download Guidelines and Code of Conduct</a>
                 <a href="" className='bg-black text-white px-16'>Announcement📢 : Click here to Download Guidelines and Code of Conduct</a>
